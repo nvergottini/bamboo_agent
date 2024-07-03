7446019b69d53338ad265a5a2c55d047cbaf0ede
Neil Vergottini <28265335+nvergottini@users.noreply.github.com>
2024-07-02 21:16:38 -0400

    Remove dependency on kayak-r9util
    Use file_line to edit wrapper.conf instead of r9util::java_properties.

--- manifests/wrapper_conf.pp
+++ manifests/wrapper_conf.pp
@@ -2,14 +2,16 @@
 # @param home The home directory of the bamboo-agent user
 # @param properties A hash of options to add to the wrapper.conf file
 define bamboo_agent::wrapper_conf (
-  $home,
-  $properties = {},
-  )
-{
-  $path = "${home}/conf/wrapper.conf"
+  Stdlib::Unixpath $home,
+  Hash[String, String] $properties = {},
+) {
+  $_path = "${home}/conf/wrapper.conf"
 
-  r9util::java_properties {$path:
-    properties => $properties
+  $properties.each |$key, $value| {
+    file_line { "${_path}:${key}":
+      path  => $_path,
+      line  => "${key}=${value}",
+      match => "^#?${key}=",
+    }
   }
-
 }
